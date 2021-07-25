f86a4a4b2710034f99cf47cb3ece2b3b85a591bc
Jess Brand <jeshbrand@gmail.com>
2021-07-25 16:20:42 +0100

    update todos

--- src/index.ts
+++ src/index.ts
@@ -9,28 +9,16 @@ function offsetCoord(coord: number, offset: number) {
 }
 const pointerTracker = new PointerTracker(element, {
   start: (pointer, event) => {
-	//   if (pointerTracker.currentPointers.length !== 0) {
-	// 	  return false
-	//   }
-	  event.preventDefault(); // event - The event related to this pointer.
-	//   console.log('inside start')
+
+	  event.preventDefault();
 	  console.log(pointer, 'pointer')
-	  console.log(event, 'start event')
 	  return true
-    // pointer - The new pointer. This pointer isn't included in this.currentPointers or
-    //    this.startPointers yet.
   },
   move: (previousPointers, changedPointers, event) => {
 	  let pointer = changedPointers[0]
 	  console.log(event, 'move event')
-	  console.log(previousPointers, 'previous')
-	  console.log(changedPointers, 'changed')
 	  element.style["top"] = `${offsetCoord(pointer.pageY, (_HEIGHT/2))}`
 	  element.style["left"] = `${offsetCoord(pointer.pageX, (_WIDTH/2))}`
-    // previousPointers - The state of the pointers before this event. This contains the same number
-    //   of pointers, in the same order, as this.currentPointers and this.startPointers.
-    // changedPointers - The pointers that have changed since the last move callback.
-    // event - The event related to the pointer changes.
   },
   end : () => {
 	  console.log('inside end')
