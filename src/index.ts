b68f5a0f6c39698d9940ec70c3ce6dc735564afb
Jess Brand <jeshbrand@gmail.com>
2021-07-25 22:56:09 +0100

    forced webpack to bundle pinchzoom, and it works now

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,55 @@
 import PointerTracker, { Pointer } from 'pointer-tracker';
+import PinchZoom from 'pinch-zoom-element';
 
+const pz = new PinchZoom(); // Force bunding of pinchzoom by webpack.
 const element: HTMLElement = document.getElementById('circle')
 const _HEIGHT = 150 // make these not hard-coded.
 const _WIDTH = 150
 
+// ensures pointer is always in middle of element during dragging
 function offsetCoord(coord: number, offset: number) {
 	return coord - offset
 }
 
-
+// puts element back in starting pos, when btn clicked
 const resetButton = document.getElementById('reset')
 resetButton.addEventListener('click',()=>{
 	element.style["top"] = '140px'
 	element.style["left"] = '300px'
-	// element.classList.remove('transform')
 })
 
-const pinchZoom = document.querySelector('.pinch-zoom');
+// once I enable this, I can't move the circle any more.
+
+// //Uncaught TypeError: document.querySelector(...).setTransform is not a function
+
+// pinchZoom.setTransform({
+// 	scale: 19,
+// 	x: 279,
+// 	y: 279,
+// 	// Fire a 'change' event if values are different to current values
+// 	allowChangeEvent: false,
+//   });
+
+// // Uncaught TypeError: document.querySelector(...).scaleTo is not a function
+
+// pinchZoom.scaleTo(0.5, {
+// 	// Transform origin. Can be a number, or string percent, eg "50%"
+// 	originX: 50,
+// 	originY: 50,
+// 	// Should the transform origin be relative to the container, or content?
+// 	relativeTo: 'content',
+// 	// Fire a 'change' event if values are different to current values
+// 	allowChangeEvent: false,
+//   });
+
 
 
 
 const pointerTracker = new PointerTracker(element, {
   start: (pointer, event) => {
+	event.preventDefault();
 	element.classList.remove('transform')
-	  event.preventDefault();
-	  return true
+	return true
   },
   move: (previousPointers, changedPointers, event) => {
 	  let pointer = changedPointers[0]
@@ -33,7 +58,6 @@ const pointerTracker = new PointerTracker(element, {
   },
   end : () => {
 	element.classList.add('transform')
-	// resetPos()
   },
   // Use raw pointer updates? Pointer events are usually synchronised to requestAnimationFrame.
   // However, if you're targeting a desynchronised canvas, then faster 'raw' updates are better.
@@ -42,7 +66,7 @@ const pointerTracker = new PointerTracker(element, {
 });
 
 
-
+//*****//
 // // State of the tracked pointers when they were pressed/touched.
 // pointerTracker.startPointers;
 // // Latest state of the tracked pointers. Contains the same number of pointers, and in the same order
