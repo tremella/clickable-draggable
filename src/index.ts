41eefe4650e1ecbf7d1dbb147d9a6e7fd41f313a
Jess Brand <jeshbrand@gmail.com>
2021-07-26 15:13:02 +0100

    did some refctor

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,38 @@ import PointerTracker, { Pointer } from 'pointer-tracker';
 
 const element: HTMLElement = document.getElementById('circle')
 const canvas: HTMLElement = document.getElementById('canvas')
-const resetButton = document.getElementById('reset')
+const resetButton: HTMLElement = document.getElementById('reset')
 
-const _HEIGHT = (parseInt(((element.style["height"]).split("px"))[0])) // make these not hard-coded.
+// global variables sourced from element's html style
+const _HEIGHT = (parseInt(((element.style["height"]).split("px"))[0]))
 const _WIDTH = (parseInt(((element.style["width"]).split("px"))[0]))
-assignPosOnStartup()
 
- // this makes bounding easier by assigning a style.
-function assignPosOnStartup(){
-	element.style["top"] = '140px'
-	element.style["left"] = '300px'
+resetBtnHandler()
+
+// makes reset button reposition element
+function resetBtnHandler(){
+	// puts element back in  starting pos, when btn clicked
+	resetButton.addEventListener('click',()=>{
+		element.style["top"] = '140px'
+		element.style["left"] = '300px'
+	})
 }
 
-// ensures pointer is always in middle of element during dragging
+// ensures pointer is in middle of element during dragging
 function offsetCoord(coord: number, offset: number) {
 	return coord - offset
 }
 
-// puts element back in  starting pos, when btn clicked
-resetButton.addEventListener('click',()=>{
-	element.style["top"] = '140px'
-	element.style["left"] = '300px'
-})
-
+// resets element position when element reaches border
 function borderGuard(child: Pointer, parent: HTMLElement) {
 	var left_border = canvas.getBoundingClientRect().x
 	var top_border = canvas.getBoundingClientRect().y
 	var bottom_border = canvas.getBoundingClientRect().height
 	var right_border = canvas.getBoundingClientRect().width
 
-	if ((child.pageY < (top_border + (_HEIGHT/2) )) //fine
+	if ((child.pageY < (top_border + (_HEIGHT/2) ))
 	|| (child.pageY > (bottom_border - (_HEIGHT/2) ))
-	|| (child.pageX < (left_border + (_HEIGHT/2) )) // fine
+	|| (child.pageX < (left_border + (_HEIGHT/2) ))
 	|| (child.pageX > (right_border - (_HEIGHT/2) ))){
 	  element.classList.add('transform')
 	  element.style["top"] = '140px'
